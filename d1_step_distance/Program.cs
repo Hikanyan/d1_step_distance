526803074d0439fa319b5f96dc800b81615cfd58
Hikanyan <hikanyan0131@gmail.com>
2022-08-02 20:33:51 +0900

    プロジェクト ファイルを追加します。

---
+++ d1_step_distance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var line = Console.ReadLine().Split(' ');
+        var d = int.Parse(line[0]);
+        var s = int.Parse(line[1]);
+        
+        var ans = 0;
+
+        ans = (d * 100000) / s;
+        Console.WriteLine(ans);
+        if (ans >= 10000)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
